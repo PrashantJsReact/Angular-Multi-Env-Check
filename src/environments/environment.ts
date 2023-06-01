a56df9cebaacdc6b8da8037e446f99a1c2603409
PrashantJsReact <prashant.ginims@gmail.com>
2023-06-01 16:23:09 +0530

    key removed

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,21 @@
 export const environment = {
   environmentName: "development",
   production: false,
+  appVersion: require('../../package.json').version + '-dev',
   cashFree: {
-    baseUrlPG: 'https://sandbox.cashfree.com/pg',
-    baseUrlSplit: 'https://test.cashfree.com',
-    'x-client-id': '137262985dc6f60df8c89d6a5a262731', 
-    'x-client-secret': '71909436af48eba30ae3098d14d8c3f110e63b52',
+    baseUrlPG: '',
+    baseUrlSplit: '',
+    'x-client-id': '', 
+    'x-client-secret': '',
   },
   firebase: {
-    apiKey: 'AIzaSyAg7cJzo2sMh-5xQ_2MWLSC-XcgayMf8pQ',
-    authDomain: 'corp-crs.firebaseapp.com',
-    projectId: 'corp-crs',
-    storageBucket: 'corp-crs.appspot.com',
-    messagingSenderId: '546442608726',
-    appId: '1:546442608726:web:8889a76537f20c51bcfbf7',
-    measurementId: 'G-DJJ3DNJW8X',
+    apiKey: '',
+    authDomain: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: '',
+    appId: '',
+    measurementId: '',
   },
 };
 
